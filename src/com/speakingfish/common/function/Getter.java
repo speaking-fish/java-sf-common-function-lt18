b18a9ec888c2ca5189c5da29d7ee74d5b9ecfda7
speaking-fish <speakingfish@gmail.com>
2015-08-19 11:52:54 +0400

    move from *value

---
+++ src/com/speakingfish/common/function/Getter.java
@@ -0,0 +1,7 @@
+package com.speakingfish.common.function;
+
+public interface Getter<T> {
+
+    T get();
+
+}
---
+++ src/com/speakingfish/common/function/Setter.java
@@ -0,0 +1,7 @@
+package com.speakingfish.common.function;
+
+public interface Setter<T> {
+
+    void set(T value);
+
+}
